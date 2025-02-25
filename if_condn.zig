3e9dd2ffdaca33da7e0e161c28f092592f686522
BitsExploited <tspamiitesh@gmail.com>
2025-02-25 21:53:14 +0530

    Adding if condition file

--- if_condn.zig
+++ if_condn.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
-    var buffer: [100]u8 = undefined; 
+    var buffer: [100]u8 = undefined;  
 
     std.debug.print("Enter number to check positive or not: ", .{});
     const input = try stdin.readUntilDelimiterOrEof(&buffer, '\n'); 
 
     if (input) |text| { 
-        const number = try std.fmt.parseInt(i32, text, 10);
+        const number = try std.fmt.parseInt(i32, text, 10); // converting string to integer
 
         if (number > 0) {
             std.debug.print("The number {} is positive.\n", .{number});
