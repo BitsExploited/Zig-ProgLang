0577a64d0eca581a7df48469b443935f9aac1fb2
BitsExploited <tspamiitesh@gmail.com>
2025-02-25 09:27:48 +0530

    Adding var assignment file

---
+++ var_assignment.zig
@@ -0,0 +1,13 @@
+const print = @import("std").debug.print; // When implemented this line it will it allows us to assign std.debug.print to the variable print so we dont have to type the whole thing
+
+pub fn main() !void {
+    // var x: i32; // This will return a error since the value is not initialised
+
+    var x: i32 = undefined; // This is the correct way to leave a variable unintialised
+
+    x = 15;
+
+    print("The value of the variable is: {d}\n", .{x});
+}
+
+
