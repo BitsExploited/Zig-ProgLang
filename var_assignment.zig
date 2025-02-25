a18426eff5be8fd464dca9bcd2a52746a2246f76
BitsExploited <tspamiitesh@gmail.com>
2025-02-25 21:23:09 +0530

    Adding var assignment file

--- var_assignment.zig
+++ var_assignment.zig
@@ -4,10 +4,13 @@ pub fn main() !void {
     // var x: i32; // This will return a error since the value is not initialised
 
     var x: i32 = undefined; // This is the correct way to leave a variable unintialised
+    const y = 15; // Immutalbe
 
+    
     x = 15;
 
     print("The value of the variable is: {d}\n", .{x});
+    print("The value of constant variable: {d}", .{y});
 }
 
 
