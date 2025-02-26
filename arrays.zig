6b62a2989e58334a3791b505ef0eb072a581e246
BitsExploited <tspamiitesh@gmail.com>
2025-02-26 15:45:09 +0530

    Adding the arrays file

--- arrays.zig
+++ arrays.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main() !void {
     const print = std.debug.print;
 
-    const arr = [2]i32{3, 2, 1};
+    const arr = [_]i32{3, 2, 1};
 
     for (arr, 0..) |num, index| {
         print("Index: {}, Numbers: {}\n", .{index, num});
