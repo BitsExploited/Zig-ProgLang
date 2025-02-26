82ad2d93e2c5dc7c321eb6f94c5b76fd0989356d
BitsExploited <tspamiitesh@gmail.com>
2025-02-26 15:48:10 +0530

    Adding the arrays file

--- arrays.zig
+++ arrays.zig
@@ -3,9 +3,16 @@ const std = @import("std");
 pub fn main() !void {
     const print = std.debug.print;
 
-    const arr = [_]i32{3, 2, 1};
+    const arr1 = [_]i32{3, 2, 1}; // Allocates the memory according to the number of elements in the array
+    const arr2 = [2]i32{5, 3, 4};
 
-    for (arr, 0..) |num, index| {
+    print("Array 1: ", .{});
+    for (arr1, 0..) |num, index| {
         print("Index: {}, Numbers: {}\n", .{index, num});
     }
+    print("Array 2: ", .{});
+    for (arr2, 0..) |num, index| {
+        print("Index: {}, Numbers: {}\n", .{index, num});
+    }
+
 }
