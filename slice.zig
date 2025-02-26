e31d5704c377f98fdeba668a41bc951ef16125f6
BitsExploited <tspamiitesh@gmail.com>
2025-02-26 16:03:13 +0530

    Adding the slice file

---
+++ slice.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const print = std.debug.print;
+
+    const arr = [_]i32{3, 2, 1, 4, 2, 4, 8, 7};
+
+    const slice1 = arr[0..6];
+    const slice2 = arr[3..6];
+
+    print("Slice 1: {any}\n", .{slice1});
+    print("Slice 2: {any}\n", .{slice2});
+}
