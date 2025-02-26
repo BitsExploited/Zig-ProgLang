15b85de8167d334cd671946d2fc7c74a07e71169
BitsExploited <tspamiitesh@gmail.com>
2025-02-26 15:40:12 +0530

    Adding the error handling file

--- errors.zig
+++ errors.zig
@@ -1,11 +1,21 @@
 const std = @import("std");
 
-fn errorOccured() !i32 {
-    if (false) return error.Oops;
-    return 42;
+fn division(a: i32, b:i32) !i32 {
+    if (b == 0) {
+        return error.DivisionByZero;
+    }
+    return @divTrunc(a, b);
 }
 
 pub fn main() !void {
-    const result = try errorOccured();
-    std.debug.print("Result: {}\n", .{result});
+    const print = std.debug.print;
+
+    const ans1 = try division(15, 5);
+    print("Division 1: {}\n", .{ans1});
+
+    const ans2 = division(10, 0) catch |err| {
+        print("Division 2: {}\n", .{err});
+        return;
+    };
+    print("Division 2: {}\n", .{ans2});
 }
