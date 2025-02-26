71d6fb6ce55f0a7d3c704cedd20b2e6d90dd9567
BitsExploited <tspamiitesh@gmail.com>
2025-02-26 16:13:57 +0530

    Adding the pointers file

--- pointers.zig
+++ pointers.zig
@@ -3,8 +3,10 @@ const std = @import("std");
 pub fn main() !void {
     const print = std.debug.print;
 
-    const x: i32 = 42;
-    const ptr: *const i32 = &x;
+    var x: i32 = 42;
+    const ptr: *i32 = &x;
+
+    ptr.* = 36;
 
     print("Value: {}\n", .{x});
     print("Value from pointer: {}\n", .{ptr.*});
